3ffa2a43ae3b344f9aa7cafbab250e1b50cd185c
Artem <Artemrid@yandex.ru>
2022-07-19 16:53:46 +0300

    Создан шифратор и дешифратор

---
+++ OOP_Lesson_7/BCoder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Lesson_7
+{
+    class BCoder : ICoder
+    {
+        public void Encode(ref string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] charMassive = str.ToCharArray();
+            foreach (char c in charMassive)
+            {
+                if(c >= 1072) // Для определения регистра
+                {
+                    if ((int)c == 2079) // Пробел обрано не возвращается, сделаю вручную....
+                    {
+                        sb.Append(' ');
+                    }
+                    else
+                    {
+                        sb.Append((char)(('а' + 31) - (c - 'а')));
+                    }
+
+                }
+                else
+                {
+                    if ((int)c == 2079)
+                    {
+                        sb.Append(' ');
+                    }
+                    else
+                    {
+                        sb.Append((char)(('А' + 31) - (c - 'А')));
+                    }
+                }
+               
+            }
+            str = sb.ToString();
+        }
+        public void Decode(ref string str)
+        {
+            Encode(ref str);
+        }
+    }
+}
--- OOP_Lesson_7/Program.cs
+++ OOP_Lesson_7/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace OOP_Lesson_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Привет Артем";
+            Console.WriteLine($"Изначальное слово: {str}");
+
+            ACoder acoder = new ACoder();
+
+            acoder.Encode(ref str);
+            Console.WriteLine($"Закодированное слово: {str}");
+            acoder.Decode(ref str);
+            Console.WriteLine($"Декодированное слово: {str}");
+
+
+            string str1 = "Привет Артем";
+            Console.WriteLine($"Изначальное слово: {str1}");
+
+            BCoder bcoder = new BCoder();
+
+            bcoder.Encode(ref str1);
+            Console.WriteLine($"Закодированное слово: {str1}");
+            bcoder.Decode(ref str1);
+            Console.WriteLine($"Декодированное слово: {str1}");
+        }
+    }
+}
